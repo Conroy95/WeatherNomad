fde5068f61898a80a40e1d1a5e934064361b6cf3
Conroy95 <leandervdneut@ziggo.nl>
2026-01-03 15:05:51 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -9,48 +9,56 @@ const locations = [
 
 async function createWeatherCard(location){
     const {name, card, condition, graphical} = location;
+
     try{
         const cardEl = document.getElementById(card);
-        const condText = await fetch(`https://wttr.in/${name}?format=%C`).then(r=>r.text());
-        document.getElementById(condition).innerText = condText;
 
-        const temp = await fetch(`https://wttr.in/${name}?format=%t`).then(r=>r.text());
-        const feels = await fetch(`https://wttr.in/${name}?format=%f`).then(r=>r.text());
-        const precip = await fetch(`https://wttr.in/${name}?format=%p`).then(r=>r.text());
-        const wind = await fetch(`https://wttr.in/${name}?format=%w`).then(r=>r.text());
-        const humidity = await fetch(`https://wttr.in/${name}?format=%h`).then(r=>r.text());
-        const sun = await fetch(`https://wttr.in/${name}?format=%S+%s`).then(r=>r.text());
+        // Één fetch naar JSON-output van wttr.in
+        const response = await fetch(`https://wttr.in/${name}?format=j1`);
+        const data = await response.json();
+
+        // Haal actuele weergegevens
+        const current = data.current_condition[0];
+        const condText = current.weatherDesc[0].value;
+        const temp = current.temp_C + "°C";
+        const feels = current.FeelsLikeC + "°C";
+        const precip = current.precipMM + " mm";
+        const wind = current.windspeedKmph + " km/h";
+        const humidity = current.humidity + "%";
+        const sun = data.weather[0].astronomy[0].sunrise + " / " + data.weather[0].astronomy[0].sunset;
+
+        document.getElementById(condition).innerText = condText;
 
         const html = `
         <div class="icon-block">
             <img src="https://img.icons8.com/fluency/48/000000/temperature.png"/>
             <span>${temp}</span>
             <div class="icon-label">Temperatuur</div>
-            <div class="meter"><div style="width:${parseInt(temp)||0*4}%"></div></div>
+            <div class="meter"><div style="width:${parseInt(current.temp_C)||0*4}%"></div></div>
         </div>
         <div class="icon-block">
             <img src="https://img.icons8.com/fluency/48/000000/thermometer.png"/>
             <span>${feels}</span>
             <div class="icon-label">Voelt als</div>
-            <div class="meter"><div style="width:${parseInt(feels)||0*4}%"></div></div>
+            <div class="meter"><div style="width:${parseInt(current.FeelsLikeC)||0*4}%"></div></div>
         </div>
         <div class="icon-block">
             <img src="https://img.icons8.com/fluency/48/000000/rain.png"/>
             <span>${precip}</span>
             <div class="icon-label">Neerslag</div>
-            <div class="meter"><div style="width:${parseFloat(precip)||0*5}%"></div></div>
+            <div class="meter"><div style="width:${parseFloat(current.precipMM)||0*5}%"></div></div>
         </div>
         <div class="icon-block">
             <img src="https://img.icons8.com/fluency/48/000000/wind.png"/>
             <span>${wind}</span>
             <div class="icon-label">Wind</div>
-            <div class="meter"><div style="width:${parseInt(wind)||0}%"></div></div>
+            <div class="meter"><div style="width:${parseInt(current.windspeedKmph)||0}%"></div></div>
         </div>
         <div class="icon-block">
             <img src="https://img.icons8.com/fluency/48/000000/humidity.png"/>
             <span>${humidity}</span>
             <div class="icon-label">Luchtvochtigheid</div>
-            <div class="meter"><div style="width:${parseInt(humidity)||0}%"></div></div>
+            <div class="meter"><div style="width:${parseInt(current.humidity)||0}%"></div></div>
         </div>
         <div class="icon-block">
             <img src="https://img.icons8.com/fluency/48/000000/sun.png"/>
@@ -61,6 +69,7 @@ async function createWeatherCard(location){
 
         document.getElementById(graphical).innerHTML = html;
 
+        // Achtergrondkleur kaart
         if(condText.toLowerCase().includes("rain")){
             cardEl.style.background = "#00a4e450";
         } else if(condText.toLowerCase().includes("cloud")){
@@ -71,7 +80,10 @@ async function createWeatherCard(location){
             cardEl.style.background = "#f0f4f820";
         }
 
-    }catch(err){console.error(err);}
+    } catch(err){
+        console.error(err);
+    }
 }
 
-locations.forEach(loc=>createWeatherCard(loc));
+// Laad alle kaarten parallel
+locations.forEach(loc => createWeatherCard(loc));
