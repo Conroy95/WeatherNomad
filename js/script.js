105671d80566157fceb2400a7734ae1c74c621aa
Conroy95 <leandervdneut@ziggo.nl>
2026-01-03 13:04:38 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
+// Functie om neerslag van wttr.in te halen
+async function getPrecip(location, elementId) {
+    try {
+        const response = await fetch(`https://wttr.in/${location}?format=%p`);
+        const data = await response.text();
+        document.getElementById(elementId).innerText = data;
+    } catch (error) {
+        document.getElementById(elementId).innerText = 'Niet beschikbaar';
+        console.error(error);
+    }
+}
 
+// Haal data op bij laden
+getPrecip('Den+Haag', 'denhaag-precip');
+getPrecip('Pijnacker', 'pijnacker-precip');
